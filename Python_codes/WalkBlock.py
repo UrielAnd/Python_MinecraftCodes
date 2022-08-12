76c865f84f2c9d8a715a1cdd3c61394b76d30f78
UrielAndrade <urielandradee@outlook.com>
2022-08-12 19:58:17 -0300

    V1.1

--- Python_codes/WalkBlock.py
+++ Python_codes/WalkBlock.py
@@ -6,11 +6,8 @@ import time
 def walk(posFinal):
      air = mc.getBlock(posFinal[0],posFinal[1]-1,posFinal[2])
      if air!=0:
-         mc.setBlocks(posFinal[0],posFinal[1]-1,posFinal[2],posFinal[0]+rang,posFinal[1]-1,posFinal[2]+rang, block)#x and z -coordinate
-         mc.setBlocks(posFinal[0],posFinal[1]-1,posFinal[2],posFinal[0]+rang,posFinal[1]-1,posFinal[2]-rang, block)#x and z -coordinate
-         mc.setBlocks(posFinal[0],posFinal[1]-1,posFinal[2],posFinal[0]-rang,posFinal[1]-1,posFinal[2]-rang, block)#x and z -coordinate
-         mc.setBlocks(posFinal[0],posFinal[1]-1,posFinal[2],posFinal[0]-rang,posFinal[1]-1,posFinal[2]+rang, block)#x and z -coordinate
-mc = Minecraft.create()   
+         mc.setBlocks(posFinal[0]-rang,posFinal[1]-1,posFinal[2]-rang,posFinal[0]+rang,posFinal[1]-1,posFinal[2]+rang, block)#x and z -coordinate
+mc = Minecraft.create()
 block = 161  #enter the block id you want from minecraft
 rang = 3   #enter the distance of the range of the blocks
 a = True
@@ -19,6 +16,4 @@ while(a==True):
      print (pos)    #info
      posFinal=list(filter(lambda x: type(x)==int, pos)) #filter to transform Vec3 into vector
      print (posFinal[0],posFinal[1],posFinal[2])  #info
-     
      walk(posFinal)     #call the function
-     time.sleep(0.09)    #sleepe
